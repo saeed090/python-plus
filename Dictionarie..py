b14254c3c261720bafe93eaa16f47bc0b2df3b5b
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-10-28 13:38:59 +0330

    Update Dictionarie..py

--- Dictionarie..py
+++ Dictionarie..py
@@ -4,3 +4,9 @@ thisdict = {
   "year": 1964
 }
 print(thisdict)
+############################################################
+Example
+Using the dict() method to make a dictionary:
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
