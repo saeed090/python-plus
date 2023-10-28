1bd5dceaf21464200396cce4d12d0cc1334d30f4
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-10-28 13:40:09 +0330

    Update Dictionarie..py

--- Dictionarie..py
+++ Dictionarie..py
@@ -10,3 +10,13 @@ Using the dict() method to make a dictionary:
 
 thisdict = dict(name = "John", age = 36, country = "Norway")
 print(thisdict)
+############################################################
+ExampleGet your own Python Server
+Get the value of the "model" key:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
