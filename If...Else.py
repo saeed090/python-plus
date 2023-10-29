8f6ef294d48180c611ebfea852c1b0fb05d38fda
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-10-29 12:50:50 +0330

    Create If...Else.py

---
+++ If...Else.py
@@ -0,0 +1,14 @@
+#Equals: a == b
+#Not Equals: a != b
+#Less than: a < b
+#Less than or equal to: a <= b
+#Greater than: a > b
+#Greater than or equal to: a >= b
+###################################################
+ExampleGet your own Python Server
+If statement:
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
