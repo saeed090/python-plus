7bf68211e39f2211a8fa4cc08cef2eeeba47a506
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-10-29 13:03:52 +0330

    Update If...Else.py

--- If...Else.py
+++ If...Else.py
@@ -12,3 +12,14 @@ a = 33
 b = 200
 if b > a:
   print("b is greater than a")
+####################################################
+Elif
+The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+
+Example
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
