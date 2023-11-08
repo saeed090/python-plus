3b7cfeb6a8bde67364c80611c4d82df0e23c2081
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-11-08 20:26:50 +0330

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -19,3 +19,11 @@ def my_function(first, sec):
   print(first + sec)
 
 my_function(a, b)
+############################################
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
