c826f49fc9949dbe48f35625faada3a9f408741b
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-11-22 20:44:30 +0330

    Create rock-paper-scissors.py

---
+++ rock-paper-scissors.py
@@ -0,0 +1,35 @@
+import random
+print("rock ...")
+print("paper ...")
+print("scissors ...")
+
+randomNumber = random.randint(0,2)
+computerMove = "rock"
+
+if randomNumber == 0:
+    computerMove = "rock"
+elif randomNumber == 1:
+    computerMove = "paper"
+elif randomNumber == 2:
+    computerMove = "scissors"
+
+player1 = input("player1 , make your move : ")
+print(f"player2 , make your move : {computerMove}")
+player2 = computerMove
+
+if player1 == "rock" and player2 == "scissors":
+    print ("player 1 wins !...")
+elif player1 == "rock" and player2 == "paper":
+    print("player 2 wins !...")
+elif player1 == "paper" and player2 == "rock":
+    print("player 1 wins !...")
+elif player1 == "paper" and player2 == "scissors":
+    print("player 2 wins !...")
+elif player1 == "scissors" and player2 == "paper":
+    print("player1 wins !...")
+elif player1 == "scissors" and player2 == "rock":
+    print("player2 wins !...")
+elif player1 == player2:
+    print ("thats a tie !...")
+else:
+    print("something went wrong !...")
