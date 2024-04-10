574e6ac6d616ee2ef0aecc4ced1716c908376c9f
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-04-10 12:16:34 +0330

    Update set.py

--- set.py
+++ set.py
@@ -1,3 +1,3 @@
 thisset = {1,2,3,4,5,6,6,6,6,7,7,7,9,9,9,8,8,8}
 print(thisset)
-# Note: the set list is unordered, meaning: the items will appear in a random order.
+# Note: the set list is unordered, meaning: the items will appear in a random order..
