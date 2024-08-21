f9cfd4bea21c253ed98cbad265c698da5655b37a
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-08-21 20:48:43 +0330

    Update for.py

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-#
+##
 nameList = ["ali" , "reza" , "sahar"]
 
 for class1 in nameList:
@@ -8,3 +8,10 @@ for class1 in nameList:
 
 for num in range(100):
     print(num)
+
+#############################################
+
+number_list = [1,2,3,4,5,6,7,8,9,10]
+
+for number_list1 in number_list:
+    print(number_list1)
