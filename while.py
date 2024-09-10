a4db289f32210aa5fe55291b096c6d046b1ee183
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-09-10 22:56:24 +0330

    while
    add while

---
+++ while.py
@@ -0,0 +1,22 @@
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 5:
+#       break
+#   i += 1
+
+
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
