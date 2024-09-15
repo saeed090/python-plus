f5d8f0112e7eb692f9a6c2203ad63bc21f9e9b4e
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-09-15 11:28:46 +0330

    first ai commit
    this is my ocr

---
+++ facedetector.py
@@ -0,0 +1,21 @@
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+detector = FaceDetector(minDetectionCon = 0.75)
+
+while True:
+    success, img = cap.read()
+
+    if success:
+        img, bboxs = detector.findFaces(img, draw = True)
+
+        cv2.imshow("Image", img)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+             break
+
+cv2.destroyAllWindows()
