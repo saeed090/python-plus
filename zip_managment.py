6bdd617e5640031fff413b513f19fcd79a838497
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-09-22 08:21:54 +0330

    first
    zip file

---
+++ zip_managment.py
@@ -0,0 +1,10 @@
+# This code is created to create zipped files
+from pathlib import Path
+from zipfile import zipfile
+
+zip = zipfile("staff.zip" , "w")  #In this section, it creates a new file and I activated the writing mode for it
+
+for path in path("staff").rglob("*.*"):
+    zip.write(path)
+
+zip.close()
